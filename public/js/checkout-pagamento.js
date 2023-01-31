5d361853176c2fa8c076c93b28293b2228b3203a
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-01-30 21:51:23 -0300

    atualizando

---
+++ public/js/checkout-pagamento.js
@@ -0,0 +1,31 @@
+const numeroCartao = document.getElementById('numero-cartao');
+
+function numeroCartaoCredito(numero) {
+    const onInputKeyDown = (event) => {
+
+        let numeroDigitado = numeroCartao.value;
+        let numeroPressionado = event.key;
+
+        event.preventDefault();
+
+        let regex = /[0-9]/;
+        let ehNumerico = regex.test(numeroPressionado);
+
+        if (ehNumerico && numeroPressionado.lenght < 20) {
+            if (
+                numeroDigitado.length == 4 ||
+                numeroDigitado.length == 9 ||
+                numeroDigitado.length == 14
+            ) {
+                input.value += " " + numeroPressionado;
+            } else {
+                input.value += numeroPressionado;
+
+            }
+        }
+
+    }
+
+    input.addEventListenner('keydown', onInputKeyDown)
+}
+
