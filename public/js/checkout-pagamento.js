0d6f03710ee363ea3189499ac0ea8d497d371299
Matheus Garcia <santos.garciamatheus@gmail.com>
2023-01-30 21:44:49 -0300

    Adicionando mudanças do projeto

--- public/js/checkout-pagamento.js
+++ public/js/checkout-pagamento.js
@@ -1,7 +1,7 @@
 const numeroCartao = document.getElementById('numero-cartao');
 
 function numeroCartaoCredito(numeroCartao) {
-    const onInputKeyDown = (event) => { 
+    const onInputKeyDown = (event) => {
 
         let numeroDigitado = numeroCartao.value;
         let numeroPressionado = event.key;
@@ -11,21 +11,35 @@ function numeroCartaoCredito(numeroCartao) {
         let regex = /[0-9]/;
         let ehNumerico = regex.test(numeroPressionado);
 
-        if (ehNumerico && numeroDigitado.lenght < 20) {
-            if (
-                numeroDigitado.length == 4 ||
-                numeroDigitado.length == 9 ||
-                numeroDigitado.length == 14
-            ) {
-                input.value += " " + numeroPressionado;
-            } else {
-                input.value += numeroPressionado;
-
-            }
+        if (ehNumerico && numeroDigitado.length < 16) {
+            alert('Informe o número do cartão com 16 dígitos')
+        } else {
+            document.submit();
         }
-
     }
-
     input.addEventListenner('keydown', onInputKeyDown)
 }
+
+
+
+
+const codigoSeguranca = document.getElementById('codigo-seguranca')
+function numeroCodigoSeguranca(codigoSeguranca) {
+    const onInputKeyDown = (event) => {
+        let numeroDigitado = codigoSeguranca.value;
+        let numeroPressionado = event.key;
+
+        event.preventDefault();
+
+        let regex = /[0-9]/;
+        let ehNumerico = regex.test(numeroPressionado);
+
+        if (ehNumerico && numeroDigitado.length != 3) {
+            alert('Informe o código de segurança com 3 dígitos')
+        } document.submit();
+    }
+    input.addEventListenner('keydown', onInputKeyDown)
+}
+
+
 
